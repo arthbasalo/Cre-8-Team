e7c0c18c4a1a6db265d9532905afd90430eb44a7
arthbasalo <73164801+arthbasalo@users.noreply.github.com>
2021-06-10 14:01:02 +0800

    updated

--- about_us.php
+++ about_us.php
@@ -17,17 +17,18 @@
           <div class="col-lg-6 pt-4 pt-lg-0 order-2 order-lg-1 content" data-aos="fade-right" data-aos-delay="100">
             <h3>CREATE TEAM (CRE-8 TEAM).</h3>
             <p class="font-italic">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
-              magna aliqua.
+            Your guide to everyday fashion. 
+            With a modern vision to break barriers in the fashion scene, CRE-8 TEAM takes the stand as the authority on everyday fashion.
             </p>
             <ul>
-              <li><i class="ri-check-double-line"></i> Ullamco laboris nisi ut aliquip ex ea commodo consequat.</li>
-              <li><i class="ri-check-double-line"></i> Duis aute irure dolor in reprehenderit in voluptate velit.</li>
-              <li><i class="ri-check-double-line"></i> Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate trideta storacalaperda mastiro dolore eu fugiat nulla pariatur.</li>
+              <li><i class="ri-check-double-line"></i> Affordable Items.</li>
+              <li><i class="ri-check-double-line"></i> High Quality Products.</li>
+              <li><i class="ri-check-double-line"></i> Free sHipping on Selected Areas.</li>
             </ul>
             <p>
-              Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-              velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident
+            Inspired by street fashion and youth culture, CRE-8 TEAM is all about being effortlessly cool. The brand schools us on what's new and 
+            makes a conscious effort to create clothes that embrace authentic individual expression, making it an easy favorite among streetwear enthusiasts. 
+            By listening to the pulse on the street, CRE-8 TEAM makes streetwear accessible for everyone without breaking the bank.
             </p>
           </div>
         </div>
--- collection.php
+++ collection.php
@@ -9,53 +9,54 @@
       	<div class="section-title">
           <h2>Collection</h2>
           <p>Check our newest collection</p>
+          <div style="color:red;">Disclaimer: Sample collections are based on the other brand, we are currently in the development stage. Thanks.</div>
         </div>
         <div class="row">
            <div class="col-lg-4 col-md-6 d-flex align-items-stretch" data-aos="zoom-in" data-aos-delay="100">
             <div class="icon-box">
               <div class="icon"><i class="bx bxl-dribbble"></i></div>
-              <h4><a href="">Lorem Ipsum</a></h4>
-              <p>Voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi</p>
+              <h4><a href="">Men's Wear</a></h4>
+              <p>Browse CRE-8 TEAM's streetwear for men. Shop men's clothing, footwear, accessories and grooming. Free 30 day return or exchange. Multiple payment options.</p>
             </div>
           </div>
 
           <div class="col-lg-4 col-md-6 d-flex align-items-stretch mt-4 mt-md-0" data-aos="zoom-in" data-aos-delay="200">
             <div class="icon-box">
-              <div class="icon"><i class="bx bx-file"></i></div>
-              <h4><a href="">Sed ut perspiciatis</a></h4>
-              <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore</p>
+            <div class="icon"><i class="bx bxl-dribbble"></i></div>
+              <h4><a href="">Ladies Wear</a></h4>
+              <p>Browse CRE-8 TEAM's best-selling streetwear for women. Shop ladies' clothing, footwear, accessories and personal care for women. Free 30 day return or exchange. Multiple payment options..</p>
             </div>
           </div>
 
           <div class="col-lg-4 col-md-6 d-flex align-items-stretch mt-4 mt-lg-0" data-aos="zoom-in" data-aos-delay="300">
             <div class="icon-box">
               <div class="icon"><i class="bx bx-tachometer"></i></div>
-              <h4><a href="">Magni Dolores</a></h4>
-              <p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia</p>
+              <h4><a href="">Accesories</a></h4>
+              <p>On a lookout for stylish accessories for men and women? Tote bags, coin purse, cap, beanies, bucket hat. Grab fashionable steals today.</p>
             </div>
           </div>
 
           <div class="col-lg-4 col-md-6 d-flex align-items-stretch mt-4" data-aos="zoom-in" data-aos-delay="100">
             <div class="icon-box">
               <div class="icon"><i class="bx bx-world"></i></div>
-              <h4><a href="">Nemo Enim</a></h4>
-              <p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis</p>
+              <h4><a href="">Collabs</a></h4>
+              <p>Streetwear for women with a twist. Your Pretty Guardian Sailor Moon outfits are now styled as graphic tees, track pants, skirts and cute accessories for women. Shop online. Multiple payment options available.</p>
             </div>
           </div>
 
           <div class="col-lg-4 col-md-6 d-flex align-items-stretch mt-4" data-aos="zoom-in" data-aos-delay="200">
             <div class="icon-box">
               <div class="icon"><i class="bx bx-slideshow"></i></div>
-              <h4><a href="">Dele cardo</a></h4>
-              <p>Quis consequatur saepe eligendi voluptatem consequatur dolor consequuntur</p>
+              <h4><a href="">New Drops</a></h4>
+              <p>Check out the latest streetwear for women and men from CRE-8 TEAM. Shop clothing, accessories, footwear, and more. Multiple payment options. Free 30 day return or exchange.</p>
             </div>
           </div>
 
           <div class="col-lg-4 col-md-6 d-flex align-items-stretch mt-4" data-aos="zoom-in" data-aos-delay="300">
             <div class="icon-box">
               <div class="icon"><i class="bx bx-arch"></i></div>
-              <h4><a href="">Divera don</a></h4>
-              <p>Modi nostrum vel laborum. Porro fugit error sit minus sapiente sit aspernatur</p>
+              <h4><a href="">Sale</a></h4>
+              <p>Score our best-selling streetwear items for women and men at a discounted price. Available for a limited time only. Online exclusive offers. We ship nationwide.</p>
             </div>
           </div>
         </div>

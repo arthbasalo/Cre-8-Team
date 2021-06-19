a9c8d3ca107e5a0677a1dd8fa851eeaed2e22286
arthbasalo <73164801+arthbasalo@users.noreply.github.com>
2021-06-19 10:44:37 +0800

    updated

--- sign_in.php
+++ sign_in.php
@@ -1,67 +1,5 @@
 <?php include("includes/header.php"); ?>
 <?php include("includes/menu.php"); ?>
-<?php 
-date_default_timezone_set("Asia/Manila");
-$dateEncoded = date("Y-m-d");
-$timeEncoded = date("h:i:s A");
-$errorMessage = "";
-if(isset($_POST['btnLogin'])){
-    $accountNumber = $_POST['txtEnterUserName'];
-    $accountPassword = $_POST['txtEnterPassword'];
-   
-    if(!VerifyUserAccount($accountNumber, $accountPassword)){
-        $errorMessage = "<p class=\"alert alert-danger\"><b>Invalid Account!.</b></p>";
-    }else{
-      global $connection;
-      $isValid = false; 
-      $query = "SELECT * FROM tbl_Account
-      INNER JOIN  tbl_Person ON tbl_Account.person_id=tbl_Person.person_id";
-      $Users = mysqli_query($connection, $query);
-      while ($User = mysqli_fetch_array($Users)) {
-        if($User['username'] == $accountNumber
-            && password_verify($accountPassword,$User['password'])
-            && $User['account_status'] == "Activated"){
-        
-            $_SESSION['personId'] = $User['person_id'];
-            $_SESSION['referenceNumber'] = $User['person_code'];
-            $_SESSION['userTypeId'] = $User['user_type_id'];
-  
-        // LOGS
-        // $category,$id(primary_key),$code,$status,$description,$added_by(person_id)
-        Create_Logs("ACCOUNT LOGIN",$User['person_id'], $User['person_code'], "LOGIN","Date and Time of Login: $dateEncoded @ $timeEncoded",$User['person_id']);
-        // END OF LOGS
-        
-        header('Location:index.php');
-                        
-        }else if($User['username'] == $accountNumber
-            && password_verify($accountPassword,$User['password'])
-            && $User['account_status'] == "Registration"){
-            $_SESSION['personId'] = $User['person_id'];
-            $_SESSION['referenceNumber'] = $User['person_code'];
-            $_SESSION['userTypeId'] = $User['user_type_id'];
-
-        // LOGS
-        // $id(primary_key),$code,$status,$description,$added_by(person_id)
-        Create_Logs("ACCOUNT LOGIN",$User['person_id'], $User['person_code'], "LOGIN","Date and Time of Login: $dateEncoded @ $timeEncoded",$User['person_id']);
-        // END OF LOGS
-
-            if($User['user_type_id'] == 7)
-              header('Location:index.php');
-
-        }else if($User['username'] == $accountNumber
-            && password_verify($accountPassword,$User['password'])
-            && $User['account_status'] == "Blocked"){
-          $errorMessage = "<p class=\"alert alert-danger\"><b>Your account has been blocked!.</b></p>";
-        }else if($User['username'] == $accountNumber
-            && password_verify($accountPassword,$User['password'])
-            && $User['account_status'] == "Deactivated"){
-          $errorMessage = "<p class=\"alert alert-warning\"><b>Your account has been Deactivated!.</b></p>";
-        }
-      }
-    }//end of ELSE
-}// end of IF
-?>
-
 <main id="main">
 
 <!-- ======= About Section ======= -->
@@ -71,7 +9,6 @@ if(isset($_POST['btnLogin'])){
         <p>Sign in</p>
     </div>
     <form method="post">
-        <?php if($errorMessage != "") echo $errorMessage; ?>
     <div class="row">
         <div class="container">
             <div class="form-group">
